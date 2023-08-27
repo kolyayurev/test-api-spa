e24d2b8a3b44fd6ed37084ff78eccfb9649a6cbf
kolyayurev <kolyayurew@gmail.com>
2023-08-27 12:08:45 +0300

    feat: simple filters
    0.5 hour

--- app/Repositories/RequestRepository.php
+++ app/Repositories/RequestRepository.php
@@ -3,22 +3,59 @@
 namespace App\Repositories;
 
 use App\Models\Request;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * @extends BaseRepository<Request>
  */
-class RequestRepository extends BaseRepository
+class RequestRepository extends BaseRepository implements RequestRepositoryContract
 {
     protected string $model = Request::class;
 
     /**
-     * @param  array<string, mixed>  $filters
-     * @return \Illuminate\Database\Eloquent\Collection<int, Request>
+     * @var string[]
      */
-    public function getByFilters(array $filters = [])
+    protected array $allowFilters = [
+        'name' => 'string',
+        'email' => 'string',
+        'status' => 'string',
+        'created_at' => 'date', // Y-m-d
+    ];
+
+    public function getByFilters(array $filters = []): Collection
     {
-        // TODO: filters and pagination
+        $this->applyFilters($filters);
 
         return $this->query->get();
+    }
+
+    public function find(int $id): ?Model
+    {
+        return $this->query->find($id);
+    }
+
+    /**
+     * @param  array<string, string>  $filters
+     */
+    private function applyFilters(array $filters = []): void
+    {
+        $query = $this->query;
+
+        foreach ($filters as $field => $value) {
+            if (in_array($field, array_keys($this->allowFilters))) {
+                switch ($this->allowFilters[$field]) {
+                    case 'date':
+                        $query->whereDate($field, date($value));
+                        break;
+                    case 'string':
+                    default:
+                        $query->where($field, $value);
+                }
+
+            }
+        }
+
+        $this->query = $query;
     }
 }
